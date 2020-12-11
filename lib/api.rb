42fa3121853b9321465a50c807947645a424fb68
stacy <stacydn23@gmail.com>
2020-12-11 14:18:58 -0500

    structured api

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,20 @@
+require_relative "../config/environment"
 class Caricom::API
-    
+
+    def initialize
+        @url = "https://restcountries.eu/rest/v2/regionalbloc/caricom"
+
+    end
+
+    def get_country
+        uri = URI.parse(@url)
+        response = Net::HTTP.get(uri)
+        countries_data = JSON.parse(response)
+        countries_data.each do |country|
+        end
+        Country.new(name: country["name"], capital: country["capital"], population: country["population"])
+    end
+
 end
+
+Caricom::API.new.get_country
