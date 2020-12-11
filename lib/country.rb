78c04aa7be516ca1fbf7a8446f08018b33e627f6
stacy <stacydn23@gmail.com>
2020-12-11 14:24:11 -0500

    structured country

--- lib/country.rb
+++ lib/country.rb
@@ -1,3 +1,18 @@
 class Caricom::Country
-    
+
+    attr_accessor :name, :capital, :population
+
+    @@all = []
+
+    def initialize(name,capital,population)
+        @name = name
+        @capital = capital
+        @population = population
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end
+
 end
