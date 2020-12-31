09c9ac8f6c83113d964b21b7e28c87c029ce4c22
stacy <stacydn23@gmail.com>
2020-12-31 16:13:15 -0500

    build two methods in the country class whilst getting familiar using iterators

--- lib/country.rb
+++ lib/country.rb
@@ -21,18 +21,54 @@ class Caricom::Country
         @@all
     end
 
-    def self.low_population
+    
+
+    # Caricom::Country.population_under(800000)
+    # Caricom::Country.population_under(450000)
+    def self.population_under(value)
         result = []
         # This method will return an array of countries with a population below 300000
         Caricom::Country.all.each do |country|
-            if country.population < 300000
-                result << country.name
+            if country.population < value
+                result << country
             end
             
         end
         return result
        
     end
+
+    def self.population_over(num)
+        result = []
+        Caricom::Country.all.each do |country|
+            if country.population > num
+                result << country
+            end
+        end
+
+        return result
+
+    end
+
+    def self.countries_with_capitals_that_start_with_p
+        Caricom::Country.all.select { |country| country.capital.start_with? 'P' }
+    end
+
+    # def self.countries_with_capitals_that_start_with(letter)
+
+    # end
+
+
+    # def self.countries_with_capitals_that_start_with_p
+    #     result = []
+    #     Caricom::Country.all.each do |country| 
+    #         if country.capital.start_with? 'P' 
+    #             result << country
+    #         end
+    #     end
+    #     return result
+    # end
+    
 
     def self.high_population
         result = []
@@ -54,6 +90,17 @@ class Caricom::Country
         return result
             
     end
+    # write a method that returns an array of the countries city and index number.
+
+    def self.city
+        result = []
+        Caricom::Country.all.each.with_index do |country, index|
+            result << "#{index}. #{country.capital}"
+        end
+        return result
+    end
+
+
     # write a method that returns an array of countries greater tan 3000000 and less than 7000000.
     def self.countries
         result = []
