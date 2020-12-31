946414d2c7e0e137d3c44e5961abb2ad8e1a988e
stacy <stacydn23@gmail.com>
2020-12-30 19:05:10 -0500

    getting comfortable with iterators

--- lib/country.rb
+++ lib/country.rb
@@ -22,22 +22,49 @@ class Caricom::Country
     end
 
     def self.low_population
-        # binding.pry
         result = []
         # This method will return an array of countries with a population below 300000
-        Caricom::Country.all.each.population < 300000
-
-        |country.population|
-    
-
-        return
-            country.population
+        Caricom::Country.all.each do |country|
+            if country.population < 300000
+                result << country.name
+            end
+            
         end
+        return result
+       
     end
 
     def self.high_population
+        result = []
         # This method will return an array of country objects with a population over 800000
+        Caricom::Country.all.each do |country|
+            if country.population > 800000
+                result << country.name
+            end
+        end
+        return result
     end
+    # what a method which returns an array of countries capital.
+
+    def self.country_capital
+        result = []
+        Caricom::Country.all.each do |country|
+            result << country.capital
+        end
+        return result
+            
+    end
+    # write a method that returns an array of countries greater tan 3000000 and less than 7000000.
+    def self.countries
+        result = []
+        Caricom::Country.all.each do |country|
+            if country.population > 300000 && country.population < 700000
+                result << country.name
+            end
+        end
+        return result
+    end
+
 end
 
 # Get really comfortable with iteration
